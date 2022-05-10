2d6b170bf683e11aa199f4df332629beb05c9baf
sally2596@gmial.com <sally2596@gmail.com>
2022-05-10 22:18:47 +0900

    0510: Mac

---
+++ 2.es6/module/util.js
@@ -0,0 +1,18 @@
+// export function cube(x) {
+//   return x * x;
+// }
+
+// export const PI = 3.14;
+
+function cube(x) {
+  return x * x;
+}
+
+const PI = 3.14;
+
+let person = {
+  name: "김재환",
+  age: 27,
+};
+
+export { cube, PI };
